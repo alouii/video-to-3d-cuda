a111d146a97a2cf85bd9f2a7273859f0414dd8f1
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:37:18 +0100

    #00898

--- cuda/registration_kernels.cu
+++ cuda/registration_kernels.cu
@@ -146,3 +146,4 @@ __global__ void computeCovarianceKernel(
             atomicAdd(&s_cov[3], src_y * tgt_x);
             atomicAdd(&s_cov[4], src_y * tgt_y);
             atomicAdd(&s_cov[5], src_y * tgt_z);
+            atomicAdd(&s_cov[6], src_z * tgt_x);
