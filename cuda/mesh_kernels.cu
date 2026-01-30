2a4c24e5d64e07c9b999695ba365d1ea7e2eb2be
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:53:15 +0100

    #01741

--- cuda/mesh_kernels.cu
+++ cuda/mesh_kernels.cu
@@ -156,3 +156,4 @@ __global__ void generateMeshKernel(
         // Add triangle
         triangles[(base_vertex + i) / 3].v0 = base_vertex + i;
         triangles[(base_vertex + i) / 3].v1 = base_vertex + i + 1;
+        triangles[(base_vertex + i) / 3].v2 = base_vertex + i + 2;
