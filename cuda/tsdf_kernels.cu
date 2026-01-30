deff1e4e64d6f0a7e7428aed8f58d8f46dfe9064
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:06:27 +0100

    #02458

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -321,3 +321,4 @@ void launchExtractSurfacePoints(
     dim3 grid(
         (config.grid_dim_x + block.x - 1) / block.x,
         (config.grid_dim_y + block.y - 1) / block.y,
+        (config.grid_dim_z + block.z - 1) / block.z
