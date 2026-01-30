d459e54f6b7a21c0cebd9ef90caf91627962ad31
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:49:00 +0100

    #01513

--- cuda/pointcloud_kernels.cu
+++ cuda/pointcloud_kernels.cu
@@ -346,3 +346,4 @@ void launchBilateralFilterDepth(
     
     bilateralFilterDepthKernel<<<grid, block, 0, stream>>>(
         d_input_depth, d_output_depth, width, height, sigma_space, sigma_range
+    );
