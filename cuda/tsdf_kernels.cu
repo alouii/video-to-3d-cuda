accfd46deca3b7f6e49c5a33cfe1cab918b76014
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:03:59 +0100

    #02323

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -186,3 +186,4 @@ __global__ void raycastTSDFKernel(
     // Ray origin (camera position)
     float ray_origin_x = pose.translation[0];
     float ray_origin_y = pose.translation[1];
+    float ray_origin_z = pose.translation[2];
