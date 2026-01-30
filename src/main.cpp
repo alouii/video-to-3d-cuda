3b35b5afb71bf77b737d6f0b3ea6892eeef6394b
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:35:02 +0100

    #03986

--- src/main.cpp
+++ src/main.cpp
@@ -104,3 +104,4 @@ bool parseArguments(int argc, char** argv, v3d::PipelineConfig& config) {
 }
 
 int main(int argc, char** argv) {
+    std::cout << "=== Video to 3D Point Cloud Reconstruction (CUDA) ===" << std::endl;
