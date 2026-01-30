023441c9723ac0c795ce6da2b8064726692873b7
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:02:24 +0100

    #02237

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -100,3 +100,4 @@ __global__ void extractSurfacePointsKernel(
 ) {
     int vx = blockIdx.x * blockDim.x + threadIdx.x;
     int vy = blockIdx.y * blockDim.y + threadIdx.y;
+    int vz = blockIdx.z * blockDim.z + threadIdx.z;
