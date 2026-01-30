7bdb9d374fcced19a46a3d670e40353855eead50
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:01:04 +0100

    #02165

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -28,3 +28,4 @@ __global__ void integrateTSDFKernel(
     float world_y = config.min_y + (vy + 0.5f) * config.voxel_size;
     float world_z = config.min_z + (vz + 0.5f) * config.voxel_size;
     
+    // Transform to camera space (inverse pose)
