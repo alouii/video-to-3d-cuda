5eefd7ff81f1a9b740b431b32bd9a2179df8df16
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:13:55 +0100

    #02848

--- include/data_structures.h
+++ include/data_structures.h
@@ -14,3 +14,4 @@ struct CameraIntrinsics {
     int width, height;
     
     CameraIntrinsics() : fx(525.0f), fy(525.0f), cx(319.5f), cy(239.5f), 
+                         width(640), height(480) {}
