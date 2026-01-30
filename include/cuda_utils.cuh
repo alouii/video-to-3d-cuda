421a1ac63756ea7bf927050300ef3c400aa8c73a
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:22:03 +0100

    #03284

--- include/cuda_utils.cuh
+++ include/cuda_utils.cuh
@@ -57,3 +57,4 @@ __device__ __forceinline__ float atomicMinFloat(float* address, float val) {
 // Warp-level reduction sum
 __device__ __forceinline__ float warpReduceSum(float val) {
     for (int offset = 16; offset > 0; offset /= 2) {
+        val += __shfl_down_sync(0xffffffff, val, offset);
