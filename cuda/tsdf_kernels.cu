a4e875050a7f687601ee206856cc676d18ce7465
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:02:29 +0100

    #02241

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -104,3 +104,4 @@ __global__ void extractSurfacePointsKernel(
     
     if (vx >= config.grid_dim_x - 1 || vy >= config.grid_dim_y - 1 || vz >= config.grid_dim_z - 1) return;
     
+    int voxel_idx = vx + vy * config.grid_dim_x + vz * config.grid_dim_x * config.grid_dim_y;
