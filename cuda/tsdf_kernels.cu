fbd0bcf1e2bcb4afd7f9c4a5ad5ebc4025acfd62
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:02:01 +0100

    #02216

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -79,3 +79,4 @@ __global__ void integrateTSDFKernel(
         int rgb_idx = pixel_idx * 3;
         voxel.r = rgb_image[rgb_idx];
         voxel.g = rgb_image[rgb_idx + 1];
+        voxel.b = rgb_image[rgb_idx + 2];
