854eefd0707c8d499157378f459fec873a6b23bb
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:06:56 +0100

    #02483

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -346,3 +346,4 @@ void launchRaycastTSDF(
     dim3 block(16, 16);
     dim3 grid((width + block.x - 1) / block.x, (height + block.y - 1) / block.y);
     
+    raycastTSDFKernel<<<grid, block, 0, stream>>>(
