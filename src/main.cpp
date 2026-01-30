885e14fd5653f5cfae3670eb5d58d3df6ed34335
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:34:22 +0100

    #03954

--- src/main.cpp
+++ src/main.cpp
@@ -72,3 +72,4 @@ bool parseArguments(int argc, char** argv, v3d::PipelineConfig& config) {
             if (i + 1 < argc) {
                 config.max_frames = std::stoi(argv[++i]);
             }
+        }
