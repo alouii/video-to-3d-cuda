04db19f8864e1c2010f600bb5ba9fea34d8fc6b4
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:34:28 +0100

    #03959

--- src/main.cpp
+++ src/main.cpp
@@ -77,3 +77,4 @@ bool parseArguments(int argc, char** argv, v3d::PipelineConfig& config) {
             if (i + 1 < argc) {
                 config.skip_frames = std::stoi(argv[++i]);
             }
+        }
