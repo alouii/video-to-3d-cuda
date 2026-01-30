d6fece376cd54d8ab4e6bab30f4b02acee3e855b
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:15:01 +0100

    #02907

--- include/data_structures.h
+++ include/data_structures.h
@@ -73,3 +73,4 @@ struct Pose {
     inline void transform(float x, float y, float z, float& out_x, float& out_y, float& out_z) const {
         out_x = rotation[0] * x + rotation[1] * y + rotation[2] * z + translation[0];
         out_y = rotation[3] * x + rotation[4] * y + rotation[5] * z + translation[1];
+        out_z = rotation[6] * x + rotation[7] * y + rotation[8] * z + translation[2];
