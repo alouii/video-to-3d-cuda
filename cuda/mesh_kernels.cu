eff4f1d71bb07d54cf46673f0b79c4efafd90ef6
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:51:00 +0100

    #01620

--- cuda/mesh_kernels.cu
+++ cuda/mesh_kernels.cu
@@ -35,3 +35,4 @@ __global__ void classifyVoxelsKernel(
         
         int corner_idx = (vx + dx) + (vy + dy) * config.grid_dim_x + (vz + dz) * config.grid_dim_x * config.grid_dim_y;
         corners[i] = voxel_grid[corner_idx].tsdf;
+        
