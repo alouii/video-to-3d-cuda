e80b60665ab985614d67c64f5373455b98998a07
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:22:05 +0100

    #03286

--- include/cuda_utils.cuh
+++ include/cuda_utils.cuh
@@ -59,3 +59,4 @@ __device__ __forceinline__ float warpReduceSum(float val) {
     for (int offset = 16; offset > 0; offset /= 2) {
         val += __shfl_down_sync(0xffffffff, val, offset);
     }
+    return val;
