daf4e4a6463ed52bb6ba400d38541082479d8b89
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:04:57 +0100

    #02376

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -239,3 +239,4 @@ __global__ void raycastTSDFKernel(
         depth_map[pixel_idx] = surface_t;
         
         // Compute normal by gradient (central differences)
+        float pos_x = ray_origin_x + world_ray_x * surface_t;
