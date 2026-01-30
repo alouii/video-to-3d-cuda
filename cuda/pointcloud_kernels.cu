f301ec8dccf3de7802ee40ef4ce19c915efcbb53
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:46:26 +0100

    #01378

--- cuda/pointcloud_kernels.cu
+++ cuda/pointcloud_kernels.cu
@@ -211,3 +211,4 @@ __global__ void statisticalOutlierRemovalKernel(
         
         const ColoredPoint& neighbor = input_points[i];
         float dx = point.x - neighbor.x;
+        float dy = point.y - neighbor.y;
