d81ef43400a1c668d24f020fda99ff8d029ab2cd
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:00:58 +0100

    #02159

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -22,3 +22,4 @@ __global__ void integrateTSDFKernel(
     int vz = blockIdx.z * blockDim.z + threadIdx.z;
     
     if (vx >= config.grid_dim_x || vy >= config.grid_dim_y || vz >= config.grid_dim_z) return;
+    
