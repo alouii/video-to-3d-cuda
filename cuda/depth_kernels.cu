01ebdcceefe747a6dc7fdbe9202b6d5eb4c3f901
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:56:46 +0100

    #01931

--- cuda/depth_kernels.cu
+++ cuda/depth_kernels.cu
@@ -134,3 +134,4 @@ __global__ void computeMatchingCostKernel(
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     
+    if (x >= width || y >= height) return;
