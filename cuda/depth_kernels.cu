4e7345143f11b3309e51b8dd640f9448a29f7ba1
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:58:18 +0100

    #02012

--- cuda/depth_kernels.cu
+++ cuda/depth_kernels.cu
@@ -215,3 +215,4 @@ __global__ void lrConsistencyCheckKernel(
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     
+    if (x >= width || y >= height) return;
