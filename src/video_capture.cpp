2ec31449ba73e81b6cc0e547b6b87f06a7e6cbcb
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:31:50 +0100

    #03815

--- src/video_capture.cpp
+++ src/video_capture.cpp
@@ -126,3 +126,4 @@ void VideoCapture::captureThread() {
         {
             std::unique_lock<std::mutex> lock(buffer_mutex_);
             buffer_cv_.wait(lock, [this] {
+                return frame_buffer_.size() < max_buffer_size_ || stop_requested_;
