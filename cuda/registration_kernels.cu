aa556b9a2d6aa2ada23fa0012933e2b7a9a33dc0
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:35:51 +0100

    #00820

--- cuda/registration_kernels.cu
+++ cuda/registration_kernels.cu
@@ -68,3 +68,4 @@ __global__ void transformPointCloudKernel(
     out_pt.nx = pose.rotation[0] * in_pt.nx + pose.rotation[1] * in_pt.ny + pose.rotation[2] * in_pt.nz;
     out_pt.ny = pose.rotation[3] * in_pt.nx + pose.rotation[4] * in_pt.ny + pose.rotation[5] * in_pt.nz;
     out_pt.nz = pose.rotation[6] * in_pt.nx + pose.rotation[7] * in_pt.ny + pose.rotation[8] * in_pt.nz;
+    
