09aa7a333915158be7409cee489bfcdb4619d11b
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:02:31 +0100

    #02243

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -106,3 +106,4 @@ __global__ void extractSurfacePointsKernel(
     
     int voxel_idx = vx + vy * config.grid_dim_x + vz * config.grid_dim_x * config.grid_dim_y;
     const TSDFVoxel& voxel = voxel_grid[voxel_idx];
+    
