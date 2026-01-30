8842f13d4401f61380223cf1fcd8c0c7e40ff953
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:05:07 +0100

    #02386

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -249,3 +249,4 @@ __global__ void raycastTSDFKernel(
             // Gradient approximation
             float nx = 0.0f, ny = 0.0f, nz = 1.0f;
             
+            int idx_px = (vx + 1) + vy * config.grid_dim_x + vz * config.grid_dim_x * config.grid_dim_y;
