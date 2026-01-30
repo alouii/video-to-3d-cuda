f6323aad569a2ae35fe134ca58262032893c0a7f
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:04:26 +0100

    #02348

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -211,3 +211,4 @@ __global__ void raycastTSDFKernel(
         }
         
         // Get TSDF value
+        int voxel_idx = vx + vy * config.grid_dim_x + vz * config.grid_dim_x * config.grid_dim_y;
