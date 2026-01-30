3c4c3a722498cc547378b6c89af1277d0358b99b
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:51:41 +0100

    #01657

--- cuda/mesh_kernels.cu
+++ cuda/mesh_kernels.cu
@@ -72,3 +72,4 @@ __global__ void generateMeshKernel(
     
     int voxel_idx = vx + vy * config.grid_dim_x + vz * config.grid_dim_x * config.grid_dim_y;
     int cube_index = voxel_types[voxel_idx];
+    
