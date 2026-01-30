1bbe514a513a6f22d44be6d4b088d573eb390367
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:47:40 +0100

    #01443

--- cuda/pointcloud_kernels.cu
+++ cuda/pointcloud_kernels.cu
@@ -276,3 +276,4 @@ __global__ void voxelDownsampleKernel(
     int vx = static_cast<int>((point.x - min_x) / voxel_size);
     int vy = static_cast<int>((point.y - min_y) / voxel_size);
     int vz = static_cast<int>((point.z - min_z) / voxel_size);
+    
