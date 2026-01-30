71e1115d89d0ce922787a74dacfc59532d92785a
alouii <aloui.lassaad@gmail.com>
2026-01-30 16:57:50 +0100

    #01989

--- cuda/depth_kernels.cu
+++ cuda/depth_kernels.cu
@@ -192,3 +192,4 @@ __global__ void medianFilterKernel(
     // Bubble sort (small arrays)
     for (int i = 0; i < count - 1; i++) {
         for (int j = 0; j < count - i - 1; j++) {
+            if (values[j] > values[j + 1]) {
