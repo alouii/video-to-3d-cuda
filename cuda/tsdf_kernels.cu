ab59d0a1d000fbc6125e6fa221e6947c3ee40a75
alouii <aloui.lassaad@gmail.com>
2026-01-30 17:00:54 +0100

    #02156

--- cuda/tsdf_kernels.cu
+++ cuda/tsdf_kernels.cu
@@ -19,3 +19,4 @@ __global__ void integrateTSDFKernel(
 ) {
     int vx = blockIdx.x * blockDim.x + threadIdx.x;
     int vy = blockIdx.y * blockDim.y + threadIdx.y;
+    int vz = blockIdx.z * blockDim.z + threadIdx.z;
